367003775688f1e2f05fe6e2c32004277bb7ea30
Pierre-Morisse <pierre.morisse2@univ-rouen.fr>
2017-06-07 14:00:12 +0200

    Code optimization

--- bin/filterOutShortAlignments.py
+++ bin/filterOutShortAlignments.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+!/usr/bin/python
 
 import sys
 
@@ -6,14 +6,15 @@ f = open(sys.argv[1])
 line = f.readline()
 
 while line[0] == "@":
-	line = f.readline()
+        line = f.readline()
 
 while line != '':
-	t = line.split("\t");
-	alLen = len(t[9])
-	if alLen >= int(sys.argv[2]):
-		with open(sys.argv[3] + "/Alignments/" + t[2], 'a') as out:
-			out.write(line[:-1])
-	line = f.readline()
+        t = line.split("\t");
+        alLen = len(t[9])
+        if alLen >= int(sys.argv[2]):
+                print(line[:-1])
+        line = f.readline()
 
 f.close()
+
+
