c3036e41e849ec5383c3c4f62fe3c47f6a4e3a81
Pierre-Morisse <pierre.morisse2@univ-rouen.fr>
2017-06-07 11:08:17 +0200

    Code optimization

--- bin/filterOutShortAlignments.py
+++ bin/filterOutShortAlignments.py
@@ -12,8 +12,10 @@ while line != '':
 	t = line.split("\t");
 	alLen = len(t[9])
 	if alLen >= int(sys.argv[2]):
-		print(line[:-1])
-		#open(sys.argv[3] + 
+		#print(line[:-1])
+		#open(sys.argv[3] +
+		with open(sys.argv[3] + "/Alignments/" + t[2], 'a') as out:
+			out.write(line[:-1])
 	line = f.readline()
 
 f.close()
